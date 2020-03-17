a326db9f814e494b4f755fe668d66006f54ceed5
amogh <amogh.sharma@gds.ey.com>
2020-03-18 01:27:04 +0530

    Add key event handler;  refactor to react class

--- src/components/SnakeCard.js
+++ src/components/SnakeCard.js
@@ -1,25 +1,74 @@
 
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
-const SnakeCard = (props) => {
-    const { theme, label } = props;
-    const k = 0;
 
-    return <Container className="mt-5">
-        <Row className="justify-content-md-center">
-            <Col md="auto">
-                <p>
-                    This is just another snake game, move up down left right to move snake!
+class SnakeCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            val: 0
+        };
+    }
+    //const { theme, label } = props;
+
+    componentDidMount() {
+        document.addEventListener('keydown', this.handleKeyPress);
+    }
+    
+    setVal = (k) => {
+        this.setState({
+          val : k
+        });
+    }
+
+    handleKeyPress = (event) => {
+        event.preventDefault();
+        console.log(event)
+        switch (event.key) {
+            case 'ArrowUp':
+                this.setVal(1);
+                break;
+            case 'ArrowDown':
+                this.setVal(2);
+                break;
+            case 'ArrowLeft':
+                this.setVal(3);
+                break;
+            case 'ArrowRight':
+                this.setVal(4);
+                break;
+            default:
+                this.setVal(0);
+                break;
+        }
+    }
+
+    
+
+    render() {
+        return (<Container className="mt-5">
+            <Row className="justify-content-md-center">
+                <Col md="auto">
+                    <p>
+                        This is just another snake game, move up down left right to move snake!
                 </p>
-            </Col>
-        </Row>
-        <Row className="justify-content-md-center">
-            <Col md="auto">
-                <h1>Here will be the snake game block</h1>
-            </Col>
-        </Row>
-    </Container>
+                </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+                <Col md="auto">
+                    <h1>Here will be the snake game block</h1>
+                </Col>
+            </Row>
+            <Row className="justify-content-md-center">
+                <Col md="auto">
+                    {this.state.val}
+                </Col>
+            </Row>
+        </Container>
+        );
+    }
+
 }
 
 
