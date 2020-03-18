d15507844466e0c881e815fdd6a8c0540e4f48ea
amogh <amogh.sharma@gds.ey.com>
2020-03-18 17:48:32 +0530

    Updates

--- precache-manifest.5059f2b32ed6c42ad68d2e2df1c88fa7.js
+++ precache-manifest.5059f2b32ed6c42ad68d2e2df1c88fa7.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e52bba5cc8642027c40b6e4081677f25",
+    "revision": "adc980b2b72374ea4023a9adea21e07e",
     "url": "/router-example/index.html"
   },
   {
-    "revision": "ab1691fc3a5696e2efde",
+    "revision": "996cf6fe88dab3db3074",
     "url": "/router-example/static/css/2.86aa6515.chunk.css"
   },
   {
-    "revision": "ab1691fc3a5696e2efde",
-    "url": "/router-example/static/js/2.7295c8cb.chunk.js"
+    "revision": "996cf6fe88dab3db3074",
+    "url": "/router-example/static/js/2.c44e2c92.chunk.js"
   },
   {
-    "revision": "3e67ba5e7a2d45279ec7",
-    "url": "/router-example/static/js/main.71906b5c.chunk.js"
+    "revision": "86aea09139b9742f1ac2",
+    "url": "/router-example/static/js/main.6f80f19f.chunk.js"
   },
   {
     "revision": "4ccd9041421c81638f1b",
