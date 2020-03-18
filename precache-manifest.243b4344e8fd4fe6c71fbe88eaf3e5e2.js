4e8ddce38cb29a70fb8f6dd3924c5853be8f68e3
amogh <amogh.sharma@gds.ey.com>
2020-03-18 17:44:21 +0530

    Updates

---
+++ precache-manifest.243b4344e8fd4fe6c71fbe88eaf3e5e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52bba5cc8642027c40b6e4081677f25",
+    "url": "/router-example/index.html"
+  },
+  {
+    "revision": "ab1691fc3a5696e2efde",
+    "url": "/router-example/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "ab1691fc3a5696e2efde",
+    "url": "/router-example/static/js/2.7295c8cb.chunk.js"
+  },
+  {
+    "revision": "3e67ba5e7a2d45279ec7",
+    "url": "/router-example/static/js/main.71906b5c.chunk.js"
+  },
+  {
+    "revision": "4ccd9041421c81638f1b",
+    "url": "/router-example/static/js/runtime~main.c715f87b.js"
+  }
+]);
