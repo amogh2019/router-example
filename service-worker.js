9b6c9e5511335dea35e2636987c6c7e15e1a3d52
amogh <amogh.sharma@gds.ey.com>
2020-03-18 18:03:09 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/router-example/precache-manifest.5059f2b32ed6c42ad68d2e2df1c88fa7.js"
+  "/router-example/precache-manifest.243b4344e8fd4fe6c71fbe88eaf3e5e2.js"
 );
 
 self.addEventListener('message', (event) => {
